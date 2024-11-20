b3be8f2aeb1e731912bd504452c5c108ac79ea4e
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-20 15:31:30 -0300

    feat:forms

---
+++ script.js
@@ -0,0 +1,12 @@
+const formClientes = document.getElementById('cadastro-cliente');
+const formProdutos = document.getElementById('cadastro-produto');
+
+function showForm(formId) {
+    if (formId === 'cadastro-cliente') {
+        formProdutos.style.display = 'none';
+        formClientes.style.display = 'flex';
+    } else if (formId === 'cadastro-produto') {
+        formProdutos.style.display = 'flex';
+        formClientes.style.display = 'none';
+    }
+}
