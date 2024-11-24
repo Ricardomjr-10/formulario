85279f63e4bdd1ff7eb2470224cb800b669192db
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-24 11:31:16 -0300

    query data in table

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const formClientes = document.getElementById('cadastro-cliente');
-const formProdutos = document.getElementById('cadastro-produto');
+// const formClientes = document.getElementById('cadastro-cliente');
+// const formProdutos = document.getElementById('cadastro-produto');
 
-function showForm(formId) {
-    if (formId === 'cadastro-cliente') {
-        formProdutos.style.display = 'none';
-        formClientes.style.display = 'flex';
-    } else if (formId === 'cadastro-produto') {
-        formProdutos.style.display = 'flex'
-        formClientes.style.display = 'none';
-    }
-}
+// function showForm(formId) {
+//     if (formId === 'cadastro-cliente') {
+//         formProdutos.style.display = 'none';
+//         formClientes.style.display = 'flex';
+//     } else if (formId === 'cadastro-produto') {
+//         formProdutos.style.display = 'flex'
+//         formClientes.style.display = 'none';
+//     }
+// }
 
 
 // criar conexao com banco de dados 
@@ -35,3 +35,13 @@ db.run(`
         INSERT INTO clientes (name, email, phone, address)
          VALUES ('Joaquim', '7mE0w@example.com', '123456789', 'Rua A, 123')
         `)
+
+        //consultar dados
+
+        db.all('SELECT * FROM clientes', (err, rows) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(rows)
+            }
+        })
